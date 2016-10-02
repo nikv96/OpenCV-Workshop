91be28c0c9f86bbcf5171aeb8c9b6ef39b7f7c6c
Nikhil <nikv96@gmail.com>
2016-10-02 16:10:55 +0800

    Added Task 7 Title

--- Task 7/HoughTransform.py
+++ Task 7/HoughTransform.py
@@ -1,23 +1,34 @@
+'''
+Task 7 - Hough Transform
+
+HoughLinesP()
+
+Detect lines in an image.
+
+HoughCircles()
+
+Detect circles in an image.
+'''
 import cv2
 import numpy as np
 
 img = cv2.imread('../resources/sample.jpg')
-image = cv2.imread('../resources/circle.png')
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,50,150,apertureSize = 3)
 minLineLength = 100
 maxLineGap = 10
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength, maxLineGap)
 for x1,y1,x2,y2 in lines[0]:
-    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+	cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
+image = cv2.imread('../resources/circle.png')
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 circles = cv2.HoughCircles(gray_image,cv2.HOUGH_GRADIENT,1,70, param1=50,param2=30,minRadius=0,maxRadius=0)
 
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
-    cv2.circle(image,(i[0],i[1]),i[2],(0,255,0),2)
+	cv2.circle(image,(i[0],i[1]),i[2],(0,255,0),2)
 
 cv2.imshow('houghlines',img)
 cv2.imshow('houghcircles',image)
